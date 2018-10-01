5ed0344cb604ed11a19aaf3e3ff11c5326f7973b
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-10-01 20:53:42 +0200

    Makes the Type class parameterized by IType

--- Editor/ViewModel/Card.cs
+++ Editor/ViewModel/Card.cs
@@ -5,18 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using NonEmptyList;
+using Pokémon;
 
 namespace Editor.ViewModel
 {
     struct Card
     {
-        public Pokémon.Type Type { get; set; }
+        public Type<IType> Type { get; set; }
         [CanBeNull] public string ImageUrl { get; set; }
         [NotNull] public string Modifier { get; set; } // e.g. 'Surfing', "Team Rocket's", etc.
         public string Pokémon { get; set; }
         public uint HP { get; set; }
-        public Pokémon.Type Weakness { get; set; }
-        public Pokémon.Type Resistance { get; set; }
+        public Type<IType> Weakness { get; set; }
+        public Type<IType> Resistance { get; set; }
         [NotNull] public NonEmptyList<Pokémon.Move> Moves { get; set; }
         // [CanBeNull] public Pokémon.Ability Ability { get; set; }
         public uint Level { get; set; }
--- Pokemon/Type.cs
+++ Pokemon/Type.cs
@@ -1,30 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Pokémon
 {
-    public enum Type
+    public sealed class Type<T> where T : IType
     {
-        Normal,
-        Fighting,
-        Flying,
-        Poison,
-        Ground,
-        Rock,
-        Bug,
-        Ghost,
-        Steel,
-        Fire,
-        Water,
-        Grass,
-        Electric,
-        Psychic,
-        Ice,
-        Dragon,
-        Dark,
-        Fairy
+        public static Type<INormal> Normal => new Type<INormal>();
+        public static Type<IFighting> Fighting => new Type<IFighting>();
+        public static Type<IFlying> Flying => new Type<IFlying>();
+        public static Type<IPoison> Poison => new Type<IPoison>();
+        public static Type<IGround> Ground => new Type<IGround>();
+        public static Type<IRock> Rock => new Type<IRock>();
+        public static Type<IBug> Bug => new Type<IBug>();
+        public static Type<IGhost> Ghost => new Type<IGhost>();
+        public static Type<ISteel> Steel => new Type<ISteel>();
+        public static Type<IFire> Fire => new Type<IFire>();
+        public static Type<IWater> Water => new Type<IWater>();
+        public static Type<IGrass> Grass => new Type<IGrass>();
+        public static Type<IElectric> Electric => new Type<IElectric>();
+        public static Type<IPsychic> Psychic => new Type<IPsychic>();
+        public static Type<IIce> Ice => new Type<IIce>();
+        public static Type<IDragon> Dragon => new Type<IDragon>();
+        public static Type<IDark> Dark => new Type<IDark>();
+        public static Type<IFairy> Fairy => new Type<IFairy>();
+
+        private Type() {}
     }
 }
