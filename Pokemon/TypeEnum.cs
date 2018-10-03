ac299de93376c0bd6e058bf2c611a27d64d285fc
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-10-03 07:38:34 +0200

    Removes accent from Pokémon, changes types to use enums for now

---
+++ Pokemon/TypeEnum.cs
@@ -0,0 +1,24 @@
+namespace Pokemon
+{
+    public enum TypeEnum
+    {
+        Normal,
+        Fighting,
+        Flying,
+        Poison,
+        Ground,
+        Rock,
+        Bug,
+        Ghost,
+        Steel,
+        Fire,
+        Water,
+        Grass,
+        Electric,
+        Psychic,
+        Ice,
+        Dragon,
+        Dark,
+        Fairy
+    }
+}
